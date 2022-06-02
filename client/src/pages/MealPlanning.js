c35b701b674b66813d85fc9eeca7d71596b75c2a
amachkel <amachkel@gmail.com>
2022-06-02 16:39:23 -0400

    add WeatherCard component and styling

--- client/src/pages/MealPlanning.js
+++ client/src/pages/MealPlanning.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React from "react";
+// import { useParams } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
 
 export default function MealPlanning() {
-  return (
-    <div className= "locations">
-     coming soon
-    </div>
-  );
+  return <div className="locations">coming soon</div>;
 }
