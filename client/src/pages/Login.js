3a4ec30b15d3fc04cd7f0fdb6eadc3ba3c343e8c
amachkel <amachkel@gmail.com>
2022-06-03 15:05:23 -0400

    adjust text to 'log in'

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -71,7 +71,7 @@ const Login = (props) => {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Log in
           </Typography>
           <Box
             component="form"
