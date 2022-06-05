0c7ab7dd0c8492d471873075ccddf94fa7c622c0
amachkel <amachkel@gmail.com>
2022-06-05 11:31:43 -0400

    local commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#33C4CF",
+      main: "#00B6C3",
+      dark: "#007F88",
+      contrastText: "#133042",
+    },
+    secondary: {
+      light: "#664B64",
+      main: "#401F3E",
+      dark: "#2C152B",
+      contrastText: "#ffffff",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
