bea2fea1822cba007868b73e3e8e7184ecc72da6
unknown <chrisponz11@yahoo.com>
2022-06-09 09:43:45 -0400

    fixed dest api call

---
+++ client/src/components/Destination/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from 'react';
+
+function Destinations(props) {
+    
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Let's go to ${search}!`)
+  }
+  const { DataisLoaded, items } = this.state;
+  if (!DataisLoaded) return 
+  <div> <h1> Please wait a moment.... </h1> </div>;
+  return (
+    <div>
+    <form className="locations" onSubmit={handleSubmit}>
+      <label>Search for your next destination:  
+        <input 
+          type="text" 
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </label>
+      <input type="submit" className="btn btn-lg btn-info m-2" id="margin-normal" margin="normal" />
+    </form>
+    <div>
+
+      <div>
+            {
+              items.map((items) => ( 
+              <div key = { items.id } >
+              
+                  </div>
+              ))
+          }
+      </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default Destinations;
