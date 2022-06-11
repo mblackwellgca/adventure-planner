c63b0ca37eca5c341c37900fb7077cfa81e0d444
Quadrilateral <amandacdulaney@gmail.com>
2022-06-11 10:16:35 -0400

    update to fetch call to pull different data

--- client/src/pages/Destinations.js
+++ client/src/pages/Destinations.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { useState } from 'react';
+var requestOptions = {
+  method: 'GET',
+  headers: {
+    'Authorization': `Basic YzNjMjAyY2Y3ZDVhMTUzZTQ5MGU2NDgxY2I3NjlhY2E6OWQ3YjM5MDQ4MmU4YjNlMmU3NWRhNDEzMWFkOWQ3NmY=`
+  },
+  redirect: 'follow'
+};
 
 function Destinations(props) {
   const [items, setItems] = useState([  ]);
   const [dataIsLoaded, setDataIsLoaded] = useState(false);
   const [search, setSearch] = useState("");
-
+  var slug = [];
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Let's go to ${search}!`)
-    var myHeader = ("Authorization:", "Basic YzNjMjAyY2Y3ZDVhMTUzZTQ5MGU2NDgxY2I3NjlhY2E6OWQ3YjM5MDQ4MmU4YjNlMmU3NWRhNDEzMWFkOWQ3NmY=");
-  var requestOptions = {
-    method: 'GET',
-    headers: {
-      'Authorization': `Basic YzNjMjAyY2Y3ZDVhMTUzZTQ5MGU2NDgxY2I3NjlhY2E6OWQ3YjM5MDQ4MmU4YjNlMmU3NWRhNDEzMWFkOWQ3NmY=`
-    },
-    // body: raw,
-    redirect: 'follow'
-  };
+    // alert(`Let's go to ${search}!`)
     fetch(`https://api.roadgoat.com/api/v2/destinations/auto_complete?q=${search}`, requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(function (data) {
+      bulkApi(data["data"][0]["attributes"]["slug"]);
+    })
     .catch(error => console.log('error', error));
+  }
+  function bulkApi(slug){
+    fetch(`https://api.roadgoat.com/api/v2/destinations/${slug}`, requestOptions)
+    .then(response => response.json())
+    .then(function (data) {
+      console.log(data)
+     })
   }
   return (
     <div>
@@ -40,7 +47,6 @@ function Destinations(props) {
             {
               items.map((items) => (
               <div key = { items.id } >
-                { items.data[0].attributes.name }
                   </div>
               ))
           }
