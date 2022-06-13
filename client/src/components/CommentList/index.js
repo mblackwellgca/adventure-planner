b85c5e1a84033ca706319f22bd152af1c57ce779
amachkel <amachkel@gmail.com>
2022-06-13 19:28:34 -0400

    cleaned up errors/warnings

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
